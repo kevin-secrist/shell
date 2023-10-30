1b511db7b6133c85b6085ae365774bfa8fb9257a
Kevin Secrist <kevin.secrist@rackspace.com>
2023-10-30 12:05:45 -0400

    update terminal settings install script to symlink entire settings folder

--- terminal/install.ps1
+++ terminal/install.ps1
@@ -1,4 +1,16 @@
-$SettingsDirectory = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
-$LocalSettingsFile = Join-Path $PSScriptRoot "settings.json"
-Move-Item "$SettingsDirectory\settings.json" "$SettingsDirectory\settings.backup.json" -Force
-New-Item -ItemType SymbolicLink -Name settings.json -Path $SettingsDirectory -Value $LocalSettingsFile
+# symlinks the current directory with the Windows Terminal settings directory
+# symlinking individual files breaks Terminal's file-watching logic
+
+$AppDirectory = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe"
+$SettingsDirectory = "$AppDirectory\LocalState"
+$BackupDirectory = "$AppDirectory\LocalState_Backup"
+$LocalSettingsDirectory = $PSScriptRoot
+
+# rename the existing LocalState directory
+Move-Item $SettingsDirectory $BackupDirectory
+
+# symlink this directory to replace the LocalState directory
+New-Item -ItemType SymbolicLink -Name "LocalState" -Path $AppDirectory -Value $LocalSettingsDirectory
+
+# copy back additional files
+Copy-Item -Path "$BackupDirectory\*" -Destination $LocalSettingsDirectory -Recurse -Exclude settings.json
